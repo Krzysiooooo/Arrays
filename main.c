e2107253798d351d44dfd5fe8385f62ca055e678
KrzysiOOO <krzysztof.poreba.jr@gmail.com>
2019-09-12 18:33:55 +0200

    First Commit

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int *inputArray() {
+    int l;
+    printf("Jak dluga tablica?\n");
+    scanf("%i", &l);
+    int *t = malloc(l * sizeof(int));
+    for (int i = 0; i < l; ++i) {
+        printf("Wprowadz %i/%i liczbe\n", i, l);
+        scanf("%i", &t[i]);
+    }
+    return t;
+}
+
+int printArray(int *v, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", *(v + i));
+    }
+}
+
+int sumArray(int t[], int s) {
+    //TODO Zadanie domowe
+}
+
+int main() {
+    int *t = inputArray();
+    printArray(t, 4);
+    return 0;
+}
